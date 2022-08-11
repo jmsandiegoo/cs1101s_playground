0319947fce406dc9adf9e4e45999c2b27b390cd7
Jm San Diego <No public email provided>
2022-08-11 11:10:25 +0800

    Changes made from Source Academy

---
+++ Week1_REC.js
@@ -0,0 +1,27 @@
+// Type your program in here!
+
+// Reflection Week 1 Elements of Programming
+
+// const x = 3;
+// const y = 4;
+
+// true ? 1 + 1 : 17;
+
+// x > 0 ? x : -x;
+
+// x === 0 ? 1 : 2;
+
+// true ? 1 : y < 1 ? 4711 : 42;
+
+
+function square(x) { 
+    return x * x;
+} 
+
+square(5);
+
+function hypothenuse(a, b) {
+    return math_sqrt(square(a) + square(b));
+}
+
+hypothenuse(3, 4);
