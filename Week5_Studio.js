39ef978ef969ad49d6951403a2d3f3fe78df817b
Jm San Diego <No public email provided>
2022-09-06 17:06:41 +0800

    Changes made from Source Academy

---
+++ Week5_Studio.js
@@ -0,0 +1,29 @@
+// Type your program in here
+function every_second(items) {
+    return is_null(items) || is_null(tail(items))
+           ? null
+           : pair(head(tail(items)), every_second(tail(tail(items))));
+}
+
+every_second(list("a", "x", "b", "y", "c", "z", "d", "e"));
+
+function every_type(items, type) {
+    if ( is_null(items) || (is_null(tail(items)) && type==="odd") ) {
+        return null;
+        
+    } else if ( is_null(tail(items)) && type==="even" ) {
+        return pair(head(items), every_type(tail(items), type));
+    } else if ( type === "even" ) {
+        return pair(head(items), every_type(tail(tail(items)), type));
+    } else {
+        return pair(head(tail(items)), every_type(tail(tail(items)), type));
+    }
+}
+
+const items = list(1,2,3,4,5,6,7);
+
+every_type(items, "odd");
+
+function sums(items) {
+    const evenList
+}
