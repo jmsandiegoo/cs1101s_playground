d8adc4fe848ecbf834af28096cbfa584f9a63966
Jm San Diego <No public email provided>
2022-10-10 21:12:02 +0800

    Changes made from Source Academy

---
+++ Week9_Studio_OutClass.js
@@ -0,0 +1,40 @@
+// function d_filter(pred, xs) {
+//     // head(xs)
+//     // set_tail?
+//     // without tail????? idk
+// }
+
+// const L = list(1, 2, 3, 4, 5, 6, 7, 8, 9, 11);
+// d_filter(x => x % 2 === 0, L); // returns [2, [4, [6, [8, null]]]]
+// L; // What is L now?
+
+// // if true
+
+// // else
+
+// // remove?
+
+
+// let a = 10;
+// function foo(x) {
+//     let b = 0;
+//     function goo(x) {
+//         let a = 30;
+//         if (x <= 2) {
+//             a = a + x;
+//             b = b + x;
+//         // Breakpoint #4
+//         } else {
+//         // Breakpoint #3
+//             goo(x - 1);
+//         }
+//     }
+//     a = a + x;
+//     b = b + x;
+//     // Breakpoint #2
+//     goo(3);
+// }
+// // Breakpoint #1
+// foo(1);
+// // Breakpoint #5
+// display('lmao');
